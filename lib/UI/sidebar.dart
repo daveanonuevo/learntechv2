ab5c4edb0357f4f2b0f467bbd0ae041cf3c37426
Dave Anonuevo <dave7999matthew@gmail.com>
2019-06-05 01:36:15 +0800

    Commit YG Stuff

--- lib/UI/sidebar.dart
+++ lib/UI/sidebar.dart
@@ -1,74 +1,79 @@
 import 'package:flutter/material.dart';
 import 'package:learntech/UI/SideBar/contactUs.dart';
+import 'package:learntech/UI/SideBar/settings.dart';
+import 'package:learntech/UI/SideBar/about.dart';
 
 class SideBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Drawer(
-				child: new ListView(
-					children: <Widget> [
-					Container(
-						height: 190,
-						decoration: BoxDecoration(
-							image: DecorationImage(
-								fit: BoxFit.cover,
-								image: ExactAssetImage('assets/images/SideBar_JewelChangi.png'),
-							),
-						),
-					),
-					new ListTile(
-						leading: new Icon(
-							Icons.settings,
-							color: Colors.black,
-							size: 40,
-						),
-						title: new Text(
-							"Settings",
-							textAlign: TextAlign.left,
-							style: new TextStyle(
-								fontSize: 30.0
-							),
-						),
-						onTap: ()=> Navigator.of(context).pop() //add on required to navigate other pages
-					),
-						new Divider(color: Colors.grey),
-						new ListTile(
-							leading: new Icon(
-								Icons.phone,
-								color: Colors.black,
-								size: 40
-							),
-							title: new Text(
-								"Contact Us",
-								textAlign: TextAlign.left,
-								style: new TextStyle(
-									fontSize: 30.0
-								),
-							),
-							onTap: ()=> Navigator.of(context).push( MaterialPageRoute(
-								builder: (context) =>
-									ContactPage()) ) //add on required to navigate other pages
-						),
-						new Divider(color: Colors.grey),
-						new ListTile(
-							leading: new Icon(
-								Icons.info,
-								color: Colors.black,
-								size: 40
-							),
-							title: new Text(
-								"About",
-								textAlign: TextAlign.left,
-								style: new TextStyle(
-									fontSize: 30.0
-								),
-							),
-							onTap: ()=> Navigator.of(context).pop() //add on required to navigate other pages
-						),
-						new Divider(color: Colors.grey),
-					],
-				),
-
-		);
+	return Drawer(
+		child: new ListView(
+		  children: <Widget> [
+		  Container(
+		  height: 190,
+		  decoration: BoxDecoration(
+			image: DecorationImage(
+			  fit: BoxFit.cover,
+			  image: ExactAssetImage('assets/images/SideBar_JewelChangi.png'),
+			),
+		  ),
+		),
+		new ListTile(
+			leading: new Icon(
+			  Icons.settings,
+			  color: Colors.black,
+			  size: 40,
+			),
+			title: new Text(
+			  "Settings",
+			  textAlign: TextAlign.left,
+			  style: new TextStyle(
+				  fontSize: 30.0
+			  ),
+			),
+			onTap: ()=> Navigator.of(context).push( MaterialPageRoute(
+		builder: (context) =>
+			Settings())	)
+	),
+	new Divider(color: Colors.grey),
+	new ListTile(
+	leading: new Icon(
+	Icons.phone,
+	color: Colors.black,
+	size: 40
+	),
+	title: new Text(
+	"Contact Us",
+	textAlign: TextAlign.left,
+	style: new TextStyle(
+	fontSize: 30.0
+	),
+	),
+	onTap: ()=> Navigator.of(context).push( MaterialPageRoute(
+	builder: (context) =>
+	ContactPage()) ) //add on required to navigate other pages
+	),
+	new Divider(color: Colors.grey),
+	new ListTile(
+	leading: new Icon(
+	Icons.info,
+	color: Colors.black,
+	size: 40
+	),
+	title: new Text(
+	"About",
+	textAlign: TextAlign.left,
+	style: new TextStyle(
+	fontSize: 30.0
+	),
+	),
+	onTap: ()=> Navigator.of(context).push(	MaterialPageRoute(
+	builder: (context) =>
+	About()) ) //add on required to navigate other pages
+	),
+	new Divider(color: Colors.grey),
+	],
+	),
+	);
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:learntech/UI/mainScreen.dart';
-import 'package:flutter/material.dart';
 
 void main() => runApp(new MaterialApp(
 	home: MainPage(

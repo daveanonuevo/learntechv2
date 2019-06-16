b0f0c315edf9720c66d483d2030ae079e86864e6
TayYuanGeng <s10184716@connect.np.edu.sg>
2019-06-16 18:10:41 +0800

    Translations Completed.

--- lib/l10n/messages_en.dart
+++ lib/l10n/messages_en.dart
@@ -22,7 +22,7 @@ class MessageLookup extends MessageLookupByLibrary {
 
   final messages = _notInlinedMessages(_notInlinedMessages);
   static _notInlinedMessages(_) => <String, Function> {
-    "contactTrans" : MessageLookupByLibrary.simpleMessage("Contact Us"),  //edit here to add translations
+    "contactTrans" : MessageLookupByLibrary.simpleMessage("Contact Us"),
     "contactContentTrans" : MessageLookupByLibrary.simpleMessage("Please provide us with any feedback or comments about the app"),
     "contactContentFeedbackTrans1" : MessageLookupByLibrary.simpleMessage("Feedback"),
     "contactContentFeedbackTrans2" : MessageLookupByLibrary.simpleMessage("Name"),
@@ -33,12 +33,10 @@ class MessageLookup extends MessageLookupByLibrary {
     "aboutContent1Title1Trans" : MessageLookupByLibrary.simpleMessage("1. All background images sourced from Unsplash.com"),
     "aboutContent2Title1Trans" : MessageLookupByLibrary.simpleMessage("2. All WhatsApp images sourced from whatsappbrand.com"),
     "aboutContentTitle2Trans" : MessageLookupByLibrary.simpleMessage("Disclaimer:"),
-
-
-
-
-
-//    "waTitle1Trans" : MessageLookupByLibrary.simpleMessage(""),
+    "aboutContent1Title2Trans" : MessageLookupByLibrary.simpleMessage("1. We are not affiliated, associated, authorized, endorsed by, or in any way officially connected with WhatsApp Inc., or any of its subsidiaries or its affiliates. We do not take responsibility for decisions/actions taken by the user based solely on the information provided in this application."),
+    "aboutContent2Title2Trans" : MessageLookupByLibrary.simpleMessage("2. We created this application with the sole intention of teaching the elderly in Singapore how to use WhatsApp and ways to be safe online."),
+    "aboutContentTailTrans" : MessageLookupByLibrary.simpleMessage("A project by Students of Ngee Ann Polytechnic"),
+    "waTitle1Trans" : MessageLookupByLibrary.simpleMessage("WhatsApp Guides"),
     "waTitle1DescTrans" : MessageLookupByLibrary.simpleMessage("Fast, Simple & Secure Messaging and Calling for Free"),
     "waTitle1Category1Trans" : MessageLookupByLibrary.simpleMessage("Installing WhatsApp"),
     "waTitle1Category1ContentTrans1" : MessageLookupByLibrary.simpleMessage("Find your phone’s App Store and open it, search for WhatsApp and install"),
@@ -53,26 +51,44 @@ class MessageLookup extends MessageLookupByLibrary {
     "waTitle1Category3ContentTrans3" : MessageLookupByLibrary.simpleMessage("Fill in the name and handphone number of an individual and tap \"Save\" when done"),
     "waTitle1Category4Trans" : MessageLookupByLibrary.simpleMessage("Sending Messages"),
     "waTitle1Category4ContentTrans1" : MessageLookupByLibrary.simpleMessage("Tap on the Message Icon, located bottom right corner"),
-    "waTitle1Category4ContentTrans2" : MessageLookupByLibrary.simpleMessage("Tap or search for the name of the person you wish to message"),
+    "waTitle1Category4ContentTrans2" : MessageLookupByLibrary.simpleMessage("Search for the name of the person you wish to message and tap on it"),
     "waTitle1Category4ContentTrans3" : MessageLookupByLibrary.simpleMessage("Tap on the bottom text field and type your message using the keyboard"),
     "waTitle1Category4ContentTrans4" : MessageLookupByLibrary.simpleMessage("When you have completed typing, tap the Arrow Icon on the bottom right corner"),
     "waTitle1Category5Trans" : MessageLookupByLibrary.simpleMessage("Sending Voice Memos"),
     "waTitle1Category5ContentTrans1" : MessageLookupByLibrary.simpleMessage("Tap on the Message Icon, located bottom right corner"),
-    "waTitle1Category5ContentTrans2" : MessageLookupByLibrary.simpleMessage("Tap or search for the name of the person you wish to send a voice message"),
+    "waTitle1Category5ContentTrans2" : MessageLookupByLibrary.simpleMessage("Search for the name of the person you wish to send a voice message and tap on it"),
     "waTitle1Category5ContentTrans3" : MessageLookupByLibrary.simpleMessage("Press and hold the microphone icon on the bottom right, and start speaking"),
     "waTitle1Category5ContentTrans4" : MessageLookupByLibrary.simpleMessage("Once you have completed, release the microphone icon and your message will be sent, or swipe left for it to be discarded"),
     "waTitle1Category6Trans" : MessageLookupByLibrary.simpleMessage("Sending Media"),
     "waTitle1Category6ContentTrans1" : MessageLookupByLibrary.simpleMessage("Tap on the Message Icon, located bottom right corner"),
-    "waTitle1Category6ContentTrans2" : MessageLookupByLibrary.simpleMessage("Tap or search for the name of the person you wish to contact"),
+    "waTitle1Category6ContentTrans2" : MessageLookupByLibrary.simpleMessage("Search for the name of the person you wish to contact and tap on it"),
     "waTitle1Category6ContentTrans3" : MessageLookupByLibrary.simpleMessage("Tap on the paperclip that is on the right of the Message Text Field"),
     "waTitle1Category6ContentTrans4" : MessageLookupByLibrary.simpleMessage("Select the media type you wish to send and follow through accordingly"),
     "waTitle1Category6ContentTrans5" : MessageLookupByLibrary.simpleMessage("Here's an example of sharing a photo"),
     "waTitle1Category6ContentTrans6" : MessageLookupByLibrary.simpleMessage("Here's an example of sharing your location \n*You may need to allow your phone permissions to do this"),
-
-
-
-
+    "waTitle1Category7Trans" : MessageLookupByLibrary.simpleMessage("Giving Voice Calls"),
+    "waTitle1Category7ContentTrans1" : MessageLookupByLibrary.simpleMessage("Tap on the Message Icon, located bottom right corner and search for the name of the person you wished to call and tap on it"),
+    "waTitle1Category7ContentTrans2" : MessageLookupByLibrary.simpleMessage("Tap on the Telephone Icon located at the top of the screen"),
+    "waTitle1Category7ContentTrans3" : MessageLookupByLibrary.simpleMessage("You may need to allow permissions"),
+    "waTitle1Category8Trans" : MessageLookupByLibrary.simpleMessage("Answering Voice Calls"),
+    "waTitle1Category8ContentTrans1" : MessageLookupByLibrary.simpleMessage("Your phone will look like this when you receive a call"),
+    "waTitle1Category8ContentTrans2" : MessageLookupByLibrary.simpleMessage("Press and hold the green button, and then swipe up to answer"),
+    "waTitle1Category8ContentTrans3" : MessageLookupByLibrary.simpleMessage("Or press the red button to decline"),
+    "waTitle1Category9Trans" : MessageLookupByLibrary.simpleMessage("Creating Group Chats"),
+    "waTitle1Category9ContentTrans1" : MessageLookupByLibrary.simpleMessage("Tap on the Message Icon, located bottom right corner"),
+    "waTitle1Category9ContentTrans2" : MessageLookupByLibrary.simpleMessage("Tap on New Group"),
+    "waTitle1Category9ContentTrans3" : MessageLookupByLibrary.simpleMessage("Select all the contacts you wish to be inside the group"),
+    "waTitle1Category9ContentTrans4" : MessageLookupByLibrary.simpleMessage("Tap on the arrow, located bottom right corner, to proceed"),
+    "waTitle1Category9ContentTrans5" : MessageLookupByLibrary.simpleMessage("Give the group a name and tap the \"tick\" to finish"),
     "safeTitle2Trans" : MessageLookupByLibrary.simpleMessage("Safety Online Tips"),
     "safeTitle2DescTrans" : MessageLookupByLibrary.simpleMessage("Tips to remain safe in the Internet World"),
+    "safeTitle2Category1Trans" : MessageLookupByLibrary.simpleMessage("Protecting your Privacy"),
+    "safeTitle2Category1ContentTrans1" : MessageLookupByLibrary.simpleMessage("Tap on the Settings icon, located top right of your phone"),
+    "safeTitle2Category1ContentTrans2" : MessageLookupByLibrary.simpleMessage("Tap Account then Privacy"),
+    "safeTitle2Category1ContentTrans3" : MessageLookupByLibrary.simpleMessage("Tap Last Seen, Select My Contacts"),
+    "safeTitle2Category1ContentTrans4" : MessageLookupByLibrary.simpleMessage("\n*This allows only those who are in your contacts to know when was the last time you used WhatsApp and settings are of personal preference"),
+    "safeTitle2Category2Trans" : MessageLookupByLibrary.simpleMessage("Identifying Scams"),
+    "safeTitle2Category3Trans" : MessageLookupByLibrary.simpleMessage("Identifying Fake News"),
+
   };
 }
--- lib/l10n/messages_zh.dart
+++ lib/l10n/messages_zh.dart
@@ -22,7 +22,7 @@ class MessageLookup extends MessageLookupByLibrary {
 
   final messages = _notInlinedMessages(_notInlinedMessages);
   static _notInlinedMessages(_) => <String, Function> {
-    "contactTrans" : MessageLookupByLibrary.simpleMessage("联系方式"),  //edit here to add translations
+    "contactTrans" : MessageLookupByLibrary.simpleMessage("联系方式"),
     "contactContentTrans" : MessageLookupByLibrary.simpleMessage("请提供个人意见（关于应用程序)"),
     "contactContentFeedbackTrans1" : MessageLookupByLibrary.simpleMessage("反馈"),
     "contactContentFeedbackTrans2" : MessageLookupByLibrary.simpleMessage("名称"),
@@ -33,11 +33,10 @@ class MessageLookup extends MessageLookupByLibrary {
     "aboutContent1Title1Trans" : MessageLookupByLibrary.simpleMessage("1. 本程序的照片来自 Unsplash.com"),
     "aboutContent2Title1Trans" : MessageLookupByLibrary.simpleMessage("2. 本程序如有 WhatsApp 的照片来自 whatsappbrand.com"),
     "aboutContentTitle2Trans" : MessageLookupByLibrary.simpleMessage("免责声明："),
-
-
-
-
-//    "waTitle1Trans" : MessageLookupByLibrary.simpleMessage(""),
+    "aboutContent1Title2Trans" : MessageLookupByLibrary.simpleMessage("1. 我们与 WhatsApp Inc. 或其任何公司，附属公司没有任何关联，授权认可或以任何方式正式有关联。对于用户者仅根据本申请中提供的信息所做出的决定或行为，我们不承担任何责任。"),
+    "aboutContent2Title2Trans" : MessageLookupByLibrary.simpleMessage("2. 我们创建本应用程序的目的是要教新加坡的年长人士如何使用 WhatsApp 以及如何安全的上网。"),
+    "aboutContentTailTrans" : MessageLookupByLibrary.simpleMessage("这项目来自义安理工学院的学生"),
+    "waTitle1Trans" : MessageLookupByLibrary.simpleMessage("WhatsApp 指南"),
     "waTitle1DescTrans" : MessageLookupByLibrary.simpleMessage("快，简单，安全，免费的简讯和通话"),
     "waTitle1Category1Trans" : MessageLookupByLibrary.simpleMessage("下载 WhatsApp"),
     "waTitle1Category1ContentTrans1" : MessageLookupByLibrary.simpleMessage("找出您电话里的 App Store，然后搜索并下载 WhatsApp"),
@@ -52,26 +51,49 @@ class MessageLookup extends MessageLookupByLibrary {
     "waTitle1Category3ContentTrans3" : MessageLookupByLibrary.simpleMessage("填上名字和电话号码，然后在按 \“保存\”"),
     "waTitle1Category4Trans" : MessageLookupByLibrary.simpleMessage("发送简讯"),
     "waTitle1Category4ContentTrans1" : MessageLookupByLibrary.simpleMessage("按右下角的按键"),
-    "waTitle1Category4ContentTrans2" : MessageLookupByLibrary.simpleMessage("按或找您想发送简讯的联络人的名字"),
+    "waTitle1Category4ContentTrans2" : MessageLookupByLibrary.simpleMessage("找您想发送简讯联络人，并按他的名字"),
     "waTitle1Category4ContentTrans3" : MessageLookupByLibrary.simpleMessage("在手机里的底部屏幕里，请按 \“输入消息\” 在白面积里然后输入您所要发送的"),
     "waTitle1Category4ContentTrans4" : MessageLookupByLibrary.simpleMessage("输入后，按右边的箭头发信息"),
     "waTitle1Category5Trans" : MessageLookupByLibrary.simpleMessage("发送录音"),
     "waTitle1Category5ContentTrans1" : MessageLookupByLibrary.simpleMessage("按右下角的按键"),
-    "waTitle1Category5ContentTrans2" : MessageLookupByLibrary.simpleMessage("按或您找想发送语音剪辑的联络人的名字"),
+    "waTitle1Category5ContentTrans2" : MessageLookupByLibrary.simpleMessage("找您想发送语音剪辑的联络人，并按他的名字"),
     "waTitle1Category5ContentTrans3" : MessageLookupByLibrary.simpleMessage("按这右下角的麦克风图签，然后开始您想说的话"),
     "waTitle1Category5ContentTrans4" : MessageLookupByLibrary.simpleMessage("当您完成想说的话，您可放开麦克风图签或向左滑动就可删除语音剪辑"),
     "waTitle1Category6Trans" : MessageLookupByLibrary.simpleMessage("发送照片与短片之类"),
     "waTitle1Category6ContentTrans1" : MessageLookupByLibrary.simpleMessage("按右下角的按键"),
-    "waTitle1Category6ContentTrans2" : MessageLookupByLibrary.simpleMessage("按或找您想联络的人"),
+    "waTitle1Category6ContentTrans2" : MessageLookupByLibrary.simpleMessage("找您想联络的人，并按他的名字"),
     "waTitle1Category6ContentTrans3" : MessageLookupByLibrary.simpleMessage("按右边的纸夹图示"),
-    "waTitle1Category6ContentTrans4" : MessageLookupByLibrary.simpleMessage("选择要发送的文件，然后贯彻始终"),
+    "waTitle1Category6ContentTrans4" : MessageLookupByLibrary.simpleMessage("选择要发送的文件，然后贯彻始终"),  //check this
     "waTitle1Category6ContentTrans5" : MessageLookupByLibrary.simpleMessage("以下是发图片的例子"),
     "waTitle1Category6ContentTrans6" : MessageLookupByLibrary.simpleMessage("以下是分享位置的例子 \n*您必须要让诚程序的到批准"),
-
-
-
-
+    "waTitle1Category7Trans" : MessageLookupByLibrary.simpleMessage("语音通话"),
+    "waTitle1Category7ContentTrans1" : MessageLookupByLibrary.simpleMessage("按右下角的按键，然后找出您想要联络的人，并按他的名字"),
+    "waTitle1Category7ContentTrans2" : MessageLookupByLibrary.simpleMessage("在屏幕的右上册，按 \“通话\” 的图示"),
+    "waTitle1Category7ContentTrans3" : MessageLookupByLibrary.simpleMessage("可能需要权限"),
+    "waTitle1Category8Trans" : MessageLookupByLibrary.simpleMessage("接语音来电"),
+    "waTitle1Category8ContentTrans1" : MessageLookupByLibrary.simpleMessage("有来电的时候，您的电话会显示："),
+    "waTitle1Category8ContentTrans2" : MessageLookupByLibrary.simpleMessage("按着绿色的按钮，然后向上滑动就能通话"),
+    "waTitle1Category8ContentTrans3" : MessageLookupByLibrary.simpleMessage("或按红色的按钮，就能拒绝通话"),
+    "waTitle1Category9Trans" : MessageLookupByLibrary.simpleMessage("创建群组聊天"),
+    "waTitle1Category9ContentTrans1" : MessageLookupByLibrary.simpleMessage("按右下角的按键"),
+    "waTitle1Category9ContentTrans2" : MessageLookupByLibrary.simpleMessage("按 \“新建群组\”"),
+    "waTitle1Category9ContentTrans3" : MessageLookupByLibrary.simpleMessage("添加成员"),
+    "waTitle1Category9ContentTrans4" : MessageLookupByLibrary.simpleMessage("在屏幕的右下角，按箭头以前往"),
+    "waTitle1Category9ContentTrans5" : MessageLookupByLibrary.simpleMessage("输入群组主题然后按 “勾”。您顺利创造群组聊天了！"),
     "safeTitle2Trans" : MessageLookupByLibrary.simpleMessage("网络安全提示"),
     "safeTitle2DescTrans" : MessageLookupByLibrary.simpleMessage("如何在网络世界保持安全的提示"),
+    "safeTitle2Category1Trans" : MessageLookupByLibrary.simpleMessage("保护个人隐私"),
+    "safeTitle2Category1ContentTrans1" : MessageLookupByLibrary.simpleMessage("在屏幕的右上角，按 \“设置\”"),
+    "safeTitle2Category1ContentTrans2" : MessageLookupByLibrary.simpleMessage("按 \“账号\” 然后再按 \“隐私\”"),
+    "safeTitle2Category1ContentTrans3" : MessageLookupByLibrary.simpleMessage("按 \“最后上线\”，然后选 \“我的联系人\”"),
+    "safeTitle2Category1ContentTrans4" : MessageLookupByLibrary.simpleMessage("\n*以上所讲的是让您的联系人知道您最后上线的时间。这些设置都能以个人调理"),
+    "safeTitle2Category2Trans" : MessageLookupByLibrary.simpleMessage("分辨出诈骗"),
+
+
+
+
+
+    "safeTitle2Category3Trans" : MessageLookupByLibrary.simpleMessage("分辨出假讯息"),
+
   };
 }
